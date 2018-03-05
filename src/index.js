18290c59d7ac7e27a475f6be2982a7352a92cd30
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2018-03-05 14:52:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+	var qwe = {};
+	for (let i = 2; i <= base; i++) {
+		while (true) {
+			if (base % i === 0) {
+				base = base / i;
+				qwe[i] = qwe[i] === undefined ? 1 : qwe[i] + 1;
+			} else {
+				break;
+			}
+		}
+	}
+
+	var array = [];
+	for (var key in qwe) {
+		var result = 0;
+		for (var i = +key; i <= number; i = i * +key) {
+			result += Math.floor(number / i);
+		}
+		array.push(Math.floor(result / qwe[key]));
+	}
+	return array.sort((a, b) => a - b)[0];
 }
